7d20210a3e9143d85e53b1d9b7d8f3e7c211515d
VG-Correa <v.victorgabriel2014@gmail.com>
2025-05-10 11:24:30 -0300

    Feito a separação de imagem em 2 partes

--- view/menu/components/image_comparator.py
+++ view/menu/components/image_comparator.py
@@ -22,12 +22,16 @@ class ImageComparator(RoundedFrame):
         self.container = tk.Frame(self, background="green")
         self.container.pack(fill="both", padx=10, pady=10)
         
-        self.image_before = tk.Frame(self.container, background="blue", width= self.container.winfo_screenwidth(), height= self.container.winfo_screenmmheight())
-        self.image_before.pack(side=tk.LEFT, padx=0, pady=0)
         
-        # self.text = tk.Label(self.image_before, text=self.container.winfo_screenheight(), background="blue")
-        # self.text.pack(side=tk.TOP, padx=0, pady=0, fill="both")
+        self.container.grid_columnconfigure(0, weight=1)
+        self.container.grid_columnconfigure(1, weight=1)
+        self.container.grid_rowconfigure(0, weight=1)
         
+        self.image_before = tk.Frame(self.container, background="blue", height=self.container.winfo_screenheight())
+        self.image_before.pack_forget()
+        self.image_before.grid(row=0, column=0, sticky="nsew", padx=2)
         
+        self.image_after = tk.Frame(self.container, background="gray", height=self.container.winfo_screenheight())
+        self.image_after.pack_forget()
+        self.image_after.grid(row=0, column=1, sticky="nsew", padx=2)
         
-        # self.container = 
