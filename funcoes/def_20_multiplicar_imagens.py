888297575d10c7a5e3911cacbaf32fae69d386c3
Audrey Bergamine <audreybergamini@outlook.com>
2025-06-19 15:58:21 -0300

    feat: adição de verificação de compatibilidade de tamanho nas imagens para multiplicação
    - Implementada verificação para garantir que as duas últimas imagens na fila tenham o mesmo tamanho e número de canais antes da multiplicação, com mensagem de erro ao usuário caso contrário. Isso melhora a robustez da função de multiplicação de imagens.

--- funcoes/def_20_multiplicar_imagens.py
+++ funcoes/def_20_multiplicar_imagens.py
@@ -5,12 +5,20 @@ from tkinter import messagebox
 
 def multiplicar_imagens(memory: ImageMemory):
     """
-    Multiplica pixel a pixel as duas últimas imagens da fila e adiciona o resultado na memória.
+    Multiplica pixel a pixel as duas últimas imagens adicionadas na fila.
+    A imagem B deve ser adicionada por último (usando 'Adicionar Imagem').
     """
     if len(memory.fila.images) < 2:
-        messagebox.showinfo("Atenção", "É necessário adicionar pelo menos mais 1 imagen para multiplicar.")
+        messagebox.showinfo("Atenção", "É necessário adicionar pelo menos mais 1 imagem para multiplicar.")
         return
+
     imagem_A = memory.fila.images[-2]
     imagem_B = memory.fila.images[-1]
+
+    # Verifica se as imagens têm o mesmo shape
+    if imagem_A.shape != imagem_B.shape:
+        messagebox.showinfo("Erro", "As imagens devem ter o mesmo tamanho e número de canais para multiplicar.")
+        return
+
     imagem_resultante = cv2.multiply(imagem_A, imagem_B)
     memory.addEdit(imagem_resultante)
