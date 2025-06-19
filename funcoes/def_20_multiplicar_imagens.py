478274efdb9f9de22b6c47e4d6b8eb0e3135ad4b
Audrey Bergamine <audreybergamini@outlook.com>
2025-06-19 15:56:07 -0300

    feat: atualização da função de multiplicação de imagens para considerar as duas últimas imagens da fila
    - Modificada a função 'multiplicar_imagens' para multiplicar pixel a pixel as duas últimas imagens na memória.
    - Adicionada verificação para garantir que existam pelo menos duas imagens antes da multiplicação, com mensagem de alerta ao usuário.

--- funcoes/def_20_multiplicar_imagens.py
+++ funcoes/def_20_multiplicar_imagens.py
@@ -1,18 +1,16 @@
 import cv2
 from service.image_memory import ImageMemory
+from tkinter import messagebox
 
 
-def multiplicar_imagens(memory: ImageMemory, mascara):
+def multiplicar_imagens(memory: ImageMemory):
     """
-    Multiplica uma imagem por uma máscara binária.
+    Multiplica pixel a pixel as duas últimas imagens da fila e adiciona o resultado na memória.
     """
-    
-    imagem = memory.getLastEdit()
-
-    # Garantir que a máscara seja binária (0 e 1)
-    mascara_binaria = (mascara > 0).astype("uint8")
-    
-    # Multiplicar a imagem pela máscara
-    imagem_resultante = cv2.multiply(imagem, mascara_binaria)
-
+    if len(memory.fila.images) < 2:
+        messagebox.showinfo("Atenção", "É necessário adicionar pelo menos duas imagens para multiplicar.")
+        return
+    imagem_A = memory.fila.images[-2]
+    imagem_B = memory.fila.images[-1]
+    imagem_resultante = cv2.multiply(imagem_A, imagem_B)
     memory.addEdit(imagem_resultante)
