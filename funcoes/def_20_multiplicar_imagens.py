4eacaa01a6dbcf249114b897fb921d3074c323d1
Audrey Bergamine <audreybergamini@outlook.com>
2025-06-19 15:56:39 -0300

    fix: correção na mensagem de alerta da função de multiplicação de imagens
    - Atualizada a mensagem exibida quando não há imagens suficientes para multiplicação, alterando "pelo menos duas imagens" para "pelo menos mais 1 imagem". Isso melhora a clareza da instrução para o usuário.

--- funcoes/def_20_multiplicar_imagens.py
+++ funcoes/def_20_multiplicar_imagens.py
@@ -8,7 +8,7 @@ def multiplicar_imagens(memory: ImageMemory):
     Multiplica pixel a pixel as duas últimas imagens da fila e adiciona o resultado na memória.
     """
     if len(memory.fila.images) < 2:
-        messagebox.showinfo("Atenção", "É necessário adicionar pelo menos duas imagens para multiplicar.")
+        messagebox.showinfo("Atenção", "É necessário adicionar pelo menos mais 1 imagen para multiplicar.")
         return
     imagem_A = memory.fila.images[-2]
     imagem_B = memory.fila.images[-1]
