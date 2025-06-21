380a7ec152c7a1a5c8583f4820afcbda2565bb0e
Audrey Bergamine <audreybergamini@outlook.com>
2025-06-21 16:03:53 -0300

    feat: ajuste na função de brilho para evitar overflow e melhorar a robustez
    - Alterado o valor padrão de k para 30, permitindo um ajuste mais significativo no brilho.
    - Implementada conversão para int16 antes da operação de ajuste de brilho, evitando overflow e garantindo que os valores permaneçam dentro do intervalo permitido.
    - Atualizado o arquivo .pyc correspondente devido a alterações no código-fonte.

--- funcoes/def_14_ajuste_de_brilho.py
+++ funcoes/def_14_ajuste_de_brilho.py
@@ -1,7 +1,8 @@
 import cv2
+import numpy as np
 from service.image_memory import ImageMemory
 
-def ajuste_de_brilho(memory: ImageMemory, k=2):
+def ajuste_de_brilho(memory: ImageMemory, k=30):
     """ Ajusta o brilho de uma imagem adicionando um valor k a cada pixel."""
 
     imagem = memory.getLastEdit()
@@ -16,6 +17,9 @@ def ajuste_de_brilho(memory: ImageMemory, k=2):
     for x in range(linhas):
         for y in range(colunas):
             for c in range(imagem.shape[2]):  # Para cada canal de cor
-                imagem[x, y, c] = min(max(imagem[x, y, c] + k, 0), 255)
+                # Converter para int16 para evitar overflow, fazer a operação e converter de volta para uint8
+                valor_atual = int(imagem[x, y, c])
+                novo_valor = min(max(valor_atual + k, 0), 255)
+                imagem[x, y, c] = np.uint8(novo_valor)
 
     memory.addEdit(imagem)
